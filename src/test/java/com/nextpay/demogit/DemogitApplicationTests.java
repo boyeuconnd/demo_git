fa90540b0944ba1336bf06e0ccb86f7d45920887
dangph <dangph1@mpos.vn>
2022-03-08 22:36:32 +0700

    init project

---
+++ src/test/java/com/nextpay/demogit/DemogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextpay.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
